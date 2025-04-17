5b1dd5437e8b0652838c68dd29c317ff668b4e1d
oLucas-o <Luc.raf.ornelas@gmail.com>
2025-04-16 22:08:44 -0300

    feat: entiti user token

---
+++ src/modules/users/database/entities/repositories/UserTokensRepositories.ts
@@ -0,0 +1,9 @@
+import { AppDataSource } from "../../../../../shared/typeorm/data-source";
+import {User_tokens} from "../userTokens"
+
+
+export const UserTokensRepositories = AppDataSource
+.getRepository(User_tokens)
+.extend({
+// Métodos personalizados serão adicionados aqui
+});
