cf6b9cb16c15a90ce04c43bab999d6013dba62ca
oLucas-o <Luc.raf.ornelas@gmail.com>
2025-04-17 11:16:00 -0300

    feat:  SendForgotPasswordEmailService.ts

---
+++ src/modules/users/services/sendForgotPasswordEmailService.ts
@@ -0,0 +1,18 @@
+import AppError from "../../../shared/erros/AppError";
+import { UsersRepository } from "../database/entities/repositories/userRepositorie";
+import { UserTokensRepositories } from "../database/entities/repositories/UserTokensRepositories";
+
+interface IForgotPassword {
+  email: string;
+}
+
+export default class SendForgotPasswordEmailService {
+  async execute({ email }: IForgotPassword): Promise<void> {
+    const user = await UsersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User not found', 404);
+    }
+    const token = await UserTokensRepositories.generate(user.id);
+  }
+}
