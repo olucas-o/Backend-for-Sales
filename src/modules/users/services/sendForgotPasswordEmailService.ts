9154ecb67d19797f419834c37703a2aca11280ed
oLucas-o <Luc.raf.ornelas@gmail.com>
2025-04-25 12:30:14 -0300

    feat: configing emailService

--- src/modules/users/services/sendForgotPasswordEmailService.ts
+++ src/modules/users/services/sendForgotPasswordEmailService.ts
@@ -1,3 +1,4 @@
+import { sendEmail } from '../../../config/email';
 import AppError from '../../../shared/erros/AppError';
 import { UsersRepository } from '../database/entities/repositories/userRepositorie';
 import { UserTokensRepositories } from '../database/entities/repositories/UserTokensRepositories';
@@ -14,5 +15,23 @@ export default class SendForgotPasswordEmailService {
       throw new AppError('User not found', 404);
     }
     const token = await UserTokensRepositories.generate(user.id);
+
+    sendEmail({
+      to: user.email,
+      subject: "My Sales - Recuperação de Senha",
+      body: `<div style="font-family: Arial, sans-serif; padding: 20px; color: #333; text-align: center; border: 2px solid #041d40; border-radius: 10px; margin: auto; width: 60%;">
+      <h1 style="color: #041d40;">Password Reset Verification Code</h1>
+      <h3 style="color: #041d40; ">Dear ${user.name},</h3>
+      <p style="font-size: 16px; color: #333;">Recover your password with this token:</p>
+      <p>
+      <strong style="border: 2px dashed #041d40; padding: 10px;
+      border-radius: 5px; font-size: 16px; color: #041d40;">${token?.token}</
+      strong>
+      You, 45 seconds ago • Uncommitted changes
+      </p>
+      I
+      <p style="margin-top: 20px;">Best regards, <br><span style="font-weight: bold; color: #041d40;">My Sales Staff</span></p>
+      </div>`
+      });
   }
 }
