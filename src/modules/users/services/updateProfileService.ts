ccd23fc64a34a02b1046faa0fc1cf2ca4d7afff1
oLucas-o <Luc.raf.ornelas@gmail.com>
2025-04-25 17:32:47 -0300

    feat: updateProfileService

---
+++ src/modules/users/services/updateProfileService.ts
@@ -0,0 +1,40 @@
+import { compare, hash } from 'bcrypt';
+import { UsersRepository } from '../database/entities/repositories/userRepositorie';
+
+interface IUpdateProfile {
+  User_ID: number;
+  email: string;
+  password: string;
+  oldPassword: string;
+}
+export default class UpdateProfileService {
+  async excute({
+    User_ID,
+    email,
+    password,
+    oldPassword,
+  }: IUpdateProfile): Promise<void> {
+    const user = await UsersRepository.findId(User_ID);
+    if (!user) {
+      throw new Error('User not find');
+    }
+    if (email) {
+      const userWithUpdatedEmail = await UsersRepository.findByEmail(email);
+      if (userWithUpdatedEmail) {
+        throw new Error('Already hast a user its email');
+      }
+      user.email = email;
+    }
+    if (password && !oldPassword) {
+      throw new Error('Old password is required');
+    }
+
+    if (password && oldPassword) {
+      const isOldPasswordValid = await compare(oldPassword, user.password);
+      if (!isOldPasswordValid) {
+        throw new Error('Old password not meet');
+      }
+      user.password = await hash(password, 10);
+    }
+  }
+}
