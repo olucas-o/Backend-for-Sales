fdf9b1d334698556d12d09e4bd9045684a716a35
oLucas-o <Luc.raf.ornelas@gmail.com>
2025-04-28 12:27:47 -0300

    feat: custmersRepositorie

---
+++ src/modules/customers/database/repositories/custumersRepositoies.ts
@@ -0,0 +1,14 @@
+import { AppDataSource } from "../../../../shared/typeorm/data-source";
+import { Customer } from "../Customers";
+
+export const UsersRepository = AppDataSource.getRepository(Customer).extend({
+  async findByEmail(email: string): Promise<Customer | null> {
+    return this.findOneBy({ email });
+  },
+  async findByName(name: string): Promise<Customer | null> {
+    return this.findOneBy({ name });
+  },
+  async findId(id: number): Promise<Customer | null> {
+    return this.findOneBy({ id });
+  },
+});
