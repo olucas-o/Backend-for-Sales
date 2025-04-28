1d1e14cf7060bca330bdccc78df4016717d10111
oLucas-o <Luc.raf.ornelas@gmail.com>
2025-04-28 15:35:30 -0300

    feat: deleteCustumerServie

---
+++ src/modules/customers/services/deleteCustumerServie.ts
@@ -0,0 +1,17 @@
+import { customersRepository } from '../database/repositories/custumersRepositoies';
+
+interface IDeleteCustomerRequest {
+  id: number,
+}
+
+export class DeleteCustomerService {
+  async execute({ id }: IDeleteCustomerRequest): Promise<void> {
+    const customer = await customersRepository.findId(id);
+
+    if (!customer) {
+      throw new Error('Customer not found');
+    }
+
+    await customersRepository.delete(id);
+  }
+}
