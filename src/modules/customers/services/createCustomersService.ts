4bcfb6846a6dc29255823d9ad5004461927e7cbe
oLucas-o <Luc.raf.ornelas@gmail.com>
2025-04-28 13:58:12 -0300

    feat: CreatCustumersService

---
+++ src/modules/customers/services/createCustomersService.ts
@@ -0,0 +1,22 @@
+import { Customer } from "../database/Customers";
+import { customersRepository } from "../database/repositories/custumersRepositoies";
+
+interface ICreateCustomerRequest {
+  name: string;
+  email: string;
+}
+
+
+export class CreateCustomerService {
+  async execute({ name, email }: ICreateCustomerRequest): Promise<Customer> {
+  const emailExists = await customersRepository.findByEmail(email);
+
+  if (emailExists) {
+  throw new Error("Email address already used");
+  }
+
+  const customer = customersRepository.create({ name, email });
+  await customersRepository.save(customer)
+  return customer;
+  }
+  }
